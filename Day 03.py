803a8fdbdfe4782182e742ea21516adc24d8a549
Veerammal V <87649528+VeerammalV@users.noreply.github.com>
2022-11-06 15:14:56 +0530

    Add files via upload

---
+++ Day 03.py
@@ -0,0 +1,18 @@
+#To find the average of three numbers
+
+number1 = int(input('Enter the first number: '))
+number2 = int(input('Enter the second number: '))
+number3 = int(input('Enter the third number: '))
+
+#to calculate the average
+average = (number1+number2+number3)/3
+
+#to display the average
+print("The average of {0}, {1} and {2} is {3}".format(number1,number2,number3,average))
+
+
+#OUTPUT
+Enter the first number: 45
+Enter the second number: 32
+Enter the third number: 78
+The average of 45, 32 and 78 is 51.666666666666664
