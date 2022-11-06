7e206dccc59a3035d829ec78b7446af275c0d8a2
Veerammal V <87649528+VeerammalV@users.noreply.github.com>
2022-11-06 15:13:04 +0530

    Update Day 01.py

--- Day 01.py
+++ Day 01.py
@@ -8,3 +8,10 @@ sum = float(Number1)+float(Number2)+float(Number3)
 
 #display the sum
 print('The sum of {0},{1} and {2} is {3}'.format(Number1,Number2,Number3,sum))
+
+
+#OUTPUT
+Enter the First Number:45
+Enter the Second Number:32
+Enter the Third Number:76
+The sum of 45,32 and 76 is 153.0
