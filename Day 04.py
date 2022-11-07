2969332011232c833edd570e0e43227f93e6dc61
Veerammal V <87649528+VeerammalV@users.noreply.github.com>
2022-11-07 21:17:14 +0530

    Add files via upload

---
+++ Day 04.py
@@ -0,0 +1,31 @@
+#deleting characters
+
+list = ['e','a','i','o','u']
+
+#removing the letter 'e'
+
+list.remove('e')
+print(list)
+
+#deleting third elements
+
+list.pop(2)
+print(list)
+
+#deleting all elements
+
+list.clear()
+print(list)
+
+
+#OUTPUT
+
+['a', 'i', 'o', 'u']
+['a', 'i', 'u']
+[]
+
+
+
+
+
+
