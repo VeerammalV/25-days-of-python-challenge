26fc59792cf6bc1f904189421dd83dee2f8b281e
Veerammal V <87649528+VeerammalV@users.noreply.github.com>
2022-11-08 21:28:25 +0530

    Add files via upload

---
+++ Day 05.py
@@ -0,0 +1,19 @@
+#importing math
+#to print the squareroot of a given number
+
+import math
+
+#calculating the squareroot of zero
+print(math.sqrt(0))
+
+#calculating the squareroot of 6
+print(math.sqrt(4))
+
+#calculating the squareroot of 5.8
+print(math.sqrt(5.8))
+
+
+#OUTPUT
+0.0
+2.0
+2.4083189157584592
