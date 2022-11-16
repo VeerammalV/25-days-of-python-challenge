e94fda0ed98de13a47dde88cec75bf1c027ae5a1
Veerammal V <87649528+VeerammalV@users.noreply.github.com>
2022-11-16 21:17:06 +0530

    Add files via upload

---
+++ Day 10.py
@@ -0,0 +1,23 @@
+#to check whether the given number is positive or negative
+
+num = int(input("Enter the integer: "))
+
+if (num > 0):
+    print("The number is positive.")
+elif (num == 0):
+    print("The number is zero.")
+else:
+    print("The number is negative")
+    
+#OUTPUT
+ 
+ Enter the integer: 14
+The number is positive.
+
+Enter the integer: -5749
+The number is negative
+
+Enter the integer: 0
+The number is zero.
+
+    
